c9538e37e97de70e156f739b7896f5d3732a9f3c
Khalid <khalidmuhammad1100@gmail.com>
2021-09-05 14:09:00 +0700

    Fix router order

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import random
 from fastapi import FastAPI, HTTPException
 
+
 app = FastAPI()
 
 with open('./book.json') as file:
@@ -11,31 +12,29 @@ with open('./thesis.json') as file:
     theses = json.load(file)
 
 @app.get("/ping")
-def read_root():
+def ping():
     return {"msg": "OK"}
 
+@app.get("/book/random")
+def get_random_book():
+    book = random.choice(books)
+    return book
 
 @app.get("/book/{id}")
-def read_item(id: int):
+def get_book_by_id(id: int):
     book = next((book for book in books if book['id'] == id), None)
     if book:
         return book
     raise HTTPException(status_code=404, detail="Book not found")
 
-@app.get("/book/random")
-def read_item():
-    book = random.choice(books)
-    return book
-
+@app.get("/thesis/random")
+def get_random_thesis():
+    thesis = random.choice(theses)
+    return thesis
 
 @app.get("/thesis/{id}")
-def read_item(id: int):
+def get_thesis_by_id(id: int):
     thesis = next((thesis for thesis in theses if thesis['id'] == id), None)
     if thesis:
         return thesis
     raise HTTPException(status_code=404, detail="Thesis not found")
-
-@app.get("/thesis/random")
-def read_item():
-    thesis = random.choice(theses)
-    return thesis
