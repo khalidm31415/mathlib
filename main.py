27bdbe52987b6df38656d4cebe575d3af17b37a3
Khalid Muhammad <khalidmuhammad1100@gmail.com>
2021-09-12 13:39:33 +0700

    Open file in each request

--- main.py
+++ main.py
@@ -5,23 +5,21 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-with open('./book.json') as file:
-    books = json.load(file)
-
-with open('./thesis.json') as file:
-    theses = json.load(file)
-
 @app.get("/ping")
 def ping():
     return {"msg": "OK"}
 
 @app.get("/book/random")
 def get_random_book():
+    with open('./book.json') as file:
+        books = json.load(file)
     book = random.choice(books)
     return book
 
 @app.get("/book/{id}")
 def get_book_by_id(id: int):
+    with open('./book.json') as file:
+        books = json.load(file)
     book = next((book for book in books if book['id'] == id), None)
     if book:
         return book
@@ -29,11 +27,15 @@ def get_book_by_id(id: int):
 
 @app.get("/thesis/random")
 def get_random_thesis():
+    with open('./thesis.json') as file:
+        theses = json.load(file)
     thesis = random.choice(theses)
     return thesis
 
 @app.get("/thesis/{id}")
 def get_thesis_by_id(id: int):
+    with open('./thesis.json') as file:
+        theses = json.load(file)
     thesis = next((thesis for thesis in theses if thesis['id'] == id), None)
     if thesis:
         return thesis
